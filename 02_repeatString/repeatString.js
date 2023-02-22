e6bc1d9776127f197f36e5be8a988939ee5b5f6d
boistei <29461756+boistei@users.noreply.github.com>
2023-02-22 19:44:33 +0530

    Solve 01 to 03

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(word,times) {
+    let final = "";
+    if (times < 0)
+        return "ERROR";
+    for(let i = 0; i < times; i++){
+        final += word;
+    }
+    return final;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let reverse = "";
+    for(let i = word.length; i >= 0; i--)
+    {
+        reverse+= word.charAt(i);
+    }
+    return reverse;
 };
 
 // Do not edit below this line
