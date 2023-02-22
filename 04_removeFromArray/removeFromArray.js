e3c67cf23d8a75e080e8dbce18f694cc6fd9b81f
boistei <29461756+boistei@users.noreply.github.com>
2023-02-22 20:06:07 +0530

    Solve 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...number) {
+    for(let i = 0; i < array.length; i++) {
+        for( let j = 0; j < number.length; j++)
+        if (array[i] === number[j]) {
+            array.splice(i,1);
+            i--;
+        }            
+    }
+    return array;
 };
 
 // Do not edit below this line
