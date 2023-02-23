7e768d657aaade0c9462954f9d294d1ec3920c89
boistei <29461756+boistei@users.noreply.github.com>
2023-02-23 10:24:40 +0530

    Solve 05, 06, 07

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(numA, numB) {
+    let result=0;
+    if (numA < 0 || numB < 0  || 
+    typeof numA !== "number" || 
+    typeof numB !== "number")
+        return "ERROR";
+    let first = (numA > numB)?numB:numA;
+    let second = (numA > numB)?numA:numB;
+    for(let i = first; i <= second; i++)
+        result += i;
+    return result;
 };
 
 // Do not edit below this line
