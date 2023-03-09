3480e4d85f9269a60bb8b412acad380798a57bc0
boistei <29461756+boistei@users.noreply.github.com>
2023-03-10 00:01:30 +0530

    Solve Ex.8 fully and attempted ex.9

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (nums) {
+  if (!nums) {
+    return 0;
+  } else {
+    let sum = 0;
+    for (let num of nums)
+      sum += parseInt(num);
+    return sum;
+  }
 };
 
-const multiply = function() {
+const multiply = function (nums) {
+  if (!nums)
+    return 0;
+  let prod = 1
+  for (let num of nums) {
+    prod *= num;
+  }
+  return prod;
+}
 
+const power = function (a, n) {
+  return a ** n;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (!num)
+    return 1;
+  let fact = 1;
+  for (let i = num; i >= 1; i--) {
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let theWord = word.toLowerCase().trim();
+    let newWord = '';
+    for (let i = theWord.length; i >= 0; i--)
+        newWord += theWord.charAt(i)
+    return newWord === theWord;
 };
 
 // Do not edit below this line
